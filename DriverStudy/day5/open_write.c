8dc77d12e82f74f202211e360d8f5ca4f7ccbac9
ilfpns <ilfpns@users.noreply.github.com>
2025-12-31 11:25:44 +0900

    open & close

--- DriverStudy/day5/open_write.c
+++ DriverStudy/day5/open_write.c
@@ -12,3 +12,20 @@ MODULE_LICENSE("GPL");
 MODULE_AUTHOR("ilfpns");
 MODULE_DESCRIPTION("Control LED");
 
+static struct file_operations fops = {
+	.owner = THIS_MODULE,
+	.open = driver_open,
+	.release = driver_close,
+	.read = driver_read,
+	.wrtie = driver_write
+}
+
+static int driver_open (struct inode *, struct file *) {
+	printk("파일 드라이버 열림!\n");
+	return 0;
+}
+
+static int driver_close (struct inode *, struct file *) {
+	printk("파일 드라이버 종료\n");
+	return 0;
+}
