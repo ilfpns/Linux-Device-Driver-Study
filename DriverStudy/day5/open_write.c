f59c02bb116e4ecc268269a572463a71c5ac06bc
ilfpns <ilfpns@users.noreply.github.com>
2026-01-01 03:58:18 +0900

    add read & write

--- DriverStudy/day5/open_write.c
+++ DriverStudy/day5/open_write.c
@@ -17,19 +17,45 @@ static struct file_operations fops = {
 	.open = driver_open,
 	.release = driver_close,
 	.read = driver_read,
-	.wrtie = driver_write
+	.write = driver_write
 }
-
-static int driver_open (struct inode *, struct file *) {
+// open & close
+static int driver_open (struct inode *Inode, struct file *File) {
 	printk("파일 드라이버 열림!\n");
 	return 0;
 }
 
-static int driver_close (struct inode *, struct file *) {
+static int driver_close (struct inode *Inode, struct file *File) {
 	printk("파일 드라이버 종료\n");
 	return 0;
 }
 
+// read & write
+ssize_t driver_read (struct file *File, char user *user_buffer, size_t count, loff_t *offs) {
+	int to_copy, not_copied, delta;
+
+	to_copy = min(count, buffer_pointer);
+	not_copied = copy_to_user(user_buffer, buffer, to_copy);
+	
+	delta = to_copy - not_copied;
+	printk("테스트 드라이버 Read함수 \n");
+	return delta;
+}
+
+static ssize_t driver_write(struct file *File, const char *user_buffer, size_t count, loff_t *offs) {
+	int to_copy, not_copied, delta;
+
+	to_copy = min(count, sizeof(buffer));
+	not_copied = copy_from_user(buffer, user_buffer, to_copy);
+	buffer_pointer = to_copy;
+
+	delta = to_copy - not_copied;
+	printk("테스트 드라이버 write함수 \n");
+
+	return delta;
+}
+
+// init & exit
 static int __init driver_init(void) {
 	printk("드이버 초기화\n");
 	return 0;
