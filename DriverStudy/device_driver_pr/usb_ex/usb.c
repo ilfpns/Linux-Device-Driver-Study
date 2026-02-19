62a7c0f20087d010f979827ca560510d9c4972e0
ilfpns <ilfpns@users.noreply.github.com>
2026-02-19 22:04:25 +0900

    usb disconnect skel

--- DriverStudy/device_driver_pr/usb_ex/usb.c
+++ DriverStudy/device_driver_pr/usb_ex/usb.c
@@ -1,19 +1,29 @@
 #include <linux/module.h>
+#include <linux/types.h>
 #include <linux/init.h>
 #include <linux/of.h>
 #include <linux/kernel.h>
 #include <linux/platform_device.h>
+#include <linux/usb.h>
+#include <linux/slab.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("ilfpns");
 
 static int usb_probe(struct usb_interface *interface, const struct usb_device_id *id);
+static void usb_disconnect(struct usb_interface *interface);
+
+struct usb_pack {
+	__u8 bulk_in;
+	__u8 bulk_out;
+};
 
 static int usb_probe(struct usb_interface *interface, const struct usb_device_id *id) {
 	struct usb_device *udev;
 	struct usb_endpoint_descriptor *bulk_in = NULL;  // USB에서 들어오는 bulk data 저장
 	struct usb_endpoint_descriptor *bulk_out = NULL; // USB로 나가는 bulk data 저
 	int retval;
+	struct usb_pack *my_usb_pack;
 
 	udev = interface_to_usbdev(interface);
 	pr_info("USB device 연결\n");
@@ -27,8 +37,17 @@ static int usb_probe(struct usb_interface *interface, const struct usb_device_id
 	retval = usb_find_common_endpoints(interface->cur_altsetting, &bulk_in, &bulk_out, NULL, NULL);
 	if (retval) {
 		pr_info("No bulk endpoints를 찾을 수 없음\n");
-		return 0;
+		return -ENODEV;
 	}
+	
+	my_usb_pack = kzalloc(sizeof(struct usb_pack), GFP_KERNEL);
+	if (!my_usb_pack) {
+		return -ENOMEM;
+	}
+
+	my_usb_pack->bulk_in = bulk_in->bEndpointAddress;
+	my_usb_pack->bulk_out = bulk_out->bEndpointAddress;
+	usb_set_intfdata(interface, my_usb_pack);	
 
 	pr_info("bulk IN  endpoint AD : 0x%02x\n", bulk_in->bEndpointAddress);   // USB -> PC
 	pr_info("bulk max packet size : %d\n", usb_endpoint_maxp(bulk_in));      // 데이터 이동 단위
@@ -36,9 +55,19 @@ static int usb_probe(struct usb_interface *interface, const struct usb_device_id
 	return 0;
 }
 
+static void usb_disconnect(struct usb_interface *interface) {
+	struct usb_pack *my_usb_pack;
+	my_usb_pack = usb_get_intfdata(interface);
+
+	kfree(my_usb_pack);
+	usb_set_intfdata(interface, NULL);
+	pr_info("USB 연결 해제 및 메모리 해제\n");	
+}
+
 static struct usb_driver usb_skel_driver = {
-	.name = "USB Skel",
-	.probe = usb_probe,
+	.name       = "USB Skel",
+	.probe      = usb_probe,
+	.disconnect = usb_disconnect,
 };
 
 module_usb_driver(usb_skel_driver);
